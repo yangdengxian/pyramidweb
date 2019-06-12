5000c256d4ae94818290c470392aa8296f81266c
yangdengxian <1053522419@qq.com>
2019-06-12 16:51:57 +0800

    Update ProxyConfiguration.java

--- src/main/java/com/pyramidweb/config/ProxyConfiguration.java
+++ src/main/java/com/pyramidweb/config/ProxyConfiguration.java
@@ -5,7 +5,12 @@ import org.springframework.beans.factory.annotation.Value;
 import java.net.InetSocketAddress;
 import java.net.Proxy;
 import java.net.SocketAddress;
-
+/**
+ * 网络代理配置
+ * @author ydx
+ * @date 2019-06-05
+ * @version 1.0.0
+ */
 public class ProxyConfiguration {
     private static String proxyHost="";
 
